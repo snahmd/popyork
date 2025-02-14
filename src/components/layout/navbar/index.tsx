fafebdcf419bcc20304c3e8e6b49438101ece03e
San <sanahmed7@gmail.com>
2025-02-15 00:45:45 +0100

    Implement responsive Navbar with mobile menu and dynamic menu items

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,12 +1,49 @@
 import { getMenu } from "@/lib/shopify";
+import { Menu } from "@/lib/shopify/types";
+import { truncate } from "fs/promises";
+import Link from "next/link";
 
 export async function Navbar() {
 
     const menu  = await getMenu("popyork-menu")
     console.log(menu)
     return (
-        <nav>
-       navvv
+        <nav className="felx items-center justify-between p-4 lg:px-8">
+            <div className="block flex-none md:hidden">
+                <MobileMenu />
+            </div>
+            <div className="flex w-full items-center">
+                <div className="flex w-full md: w-1/3">
+                <Link href={"/"} prefetch={true} className="mr-2 flex w-full items-center justify-center md:w-auto ld:mr-6">
+                <Logo />
+                <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
+                    {SITE_NAME}
+                </div>
+                </Link>
+                {menu.length > 0 ? (
+                    <ul className="hidden gap-6 text-sm md:flex md:items-center" >
+                        
+                            {menu.map((item:Menu,) => (
+                                <li key={item.title}>
+                                    <Link href={item.path} prefetch={true} className="text-gray-700 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300" >
+                                        <a>{item.title}</a>
+                                    </Link>
+                                </li>
+                           ) )}
+                        
+                    </ul>
+                ) : null}
+                </div>
+                <div className="hidden  justify-center md:flex md:w-1/3">
+                    <Search />
+
+                </div>
+                <div className="flex  justify-end md:flex md:w-1/3">
+                    <CartModal />
+
+                </div>
+            </div>
+       
         </nav>
     );
 }
---
+++ src/components/layout/navbar/mobile-menu.tsx
@@ -0,0 +1,9 @@
+export { Menu } from "@/app/lib/shopify/types";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
+export default function MobileMenu({menu}: {menu: Menu[]}) {
+    return (
+        <button onClick={}>
+            <Bars3Icon />
+        </button>
+    )
