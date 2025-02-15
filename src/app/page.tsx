e5f52dd143b4e14a629d0070bfb898dca74337c2
San <sanahmed7@gmail.com>
2025-02-16 00:48:50 +0100

    Add Footer component and integrate it into layout; update global styles for better accessibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Navbar } from "@/components/layout/navbar";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
+    
       <h1>Home</h1>
     </div>
   );
